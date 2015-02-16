f699ce779b0050926b5c538a7970a2d1ad93ba1e
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2015-02-16 14:05:24 +0300

    The lost commit.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new(title: 'Qweqwe',
+                          description: 'asdfg',
+                          image_url: 'qqq.gif')
+    product.price = -1
+    assert product.invalid?
+    assert_equal 'must be greater than or equal to 0.01',
+                 product.errors[:price].join('; ')
+    product.price = 0
+    assert_equal 'must be greater than or equal to 0.01',
+                 product.errors[:price].join('; ')
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title: 'Sdddd',
+                description: 'sadasdasd',
+                price: 1,
+                image_url: image_url)
+  end
+
+  test "image url" do
+    ok = %w{fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+            http://a.b.c/x/y/z/fred.gif}
+    bad = %w{fred.gf fred.jg fred.pg FRED.JPG/more
+            http://asd.asd.asd/x/d/f/s/fred.Jpg.more}
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert !new_product(name).valid?, "#{name} shouldn't be valid"
+    end
+  end
 end
