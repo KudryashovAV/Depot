e3f9efcb8de2c66856e2ac93124386c74679b6ef
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2015-02-18 14:09:34 +0300

    Created cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  has_many :line_item
+
+  before_destroy :ensure_not_refereced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -6,4 +10,13 @@ class Product < ActiveRecord::Base
             with: %r{\.(gif|jpg|png)\z}i,
             message: "URL must point to a picture format GIF, JPG or PNG "
             }
+
+  private
+
+  def ensure_not_refereced_by_any_line_item
+    unless line_item.empty?
+      errors.add(:base, "There are headings.")
+      false
+    end
+  end
 end
